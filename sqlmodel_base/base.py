85554e21697dd3e8ec6c0fddf92ea643030fec0a
Waylon S. Walker <waylon@waylonwalker.com>
2024-02-29 15:05:57 -0600

    wip

--- sqlmodel_base/base.py
+++ sqlmodel_base/base.py
@@ -127,13 +127,6 @@ def create_heroes():
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador").create()
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48).create()
 
-    # with Session(engine) as session:
-    #     session.add(hero_1)
-    #     session.add(hero_2)
-    #     session.add(hero_3)
-    #
-    #     session.commit()
-
 
 def page_heroes():
     next_page = 1
